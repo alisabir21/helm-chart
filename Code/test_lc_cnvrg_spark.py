8350f273a44a75e2510981060d7ad1e8ea8815d7
933598 <alihasan.a.sabir@seagate.com>
2022-11-07 15:29:23 +0530

    updated code

--- Code/test_lc_cnvrg_spark.py
+++ Code/test_lc_cnvrg_spark.py
@@ -20,4 +20,4 @@ print(spark.conf.get("spark.hadoop.fs.s3a.endpoint"))
 df_load = spark.read.csv('s3a://lc-stx-ap1-edp-media-dl-prod-raw/raw.db/sputter_slot_machine_svc20/batch_id=1656060651074/StationPressure220621022600_MAINT_MBI_VB61U_11May22.csv')
 df_load.show(10)
 
-df_load.write.csv('s3a://lc-stx-ap1-edp-media-dl-prod-raw/raw.db/sputter_slot_machine_svc20/batch_id=1656060651123/StationPressure220621022600_MAINT_MBI_VB61U_11May22.csv')
+df_load.write.csv('s3a://lc-stx-ap1-edp-media-dl-prod-raw/raw.db/sputter_slot_machine_svc20/batch_id=123/StationPressure220621022600_MAINT_MBI_VB61U_11May22.csv')
